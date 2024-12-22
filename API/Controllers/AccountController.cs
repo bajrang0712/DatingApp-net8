f02982b738a56033e0fb602b43bfbde15c2e4a74
bajrang0712 <bajrang.sutar0712@gmail.com>
2024-12-22 16:16:38 +0000

    End of section 8

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -21,7 +21,9 @@ public class AccountController(DataContext context, ITokenService tokenService) 
         {
                 return BadRequest("Username is taken");
         }
-        using var hmac = new HMACSHA512();
+
+        return Ok();
+        /* using var hmac = new HMACSHA512();
 
         var user = new AppUser
         {
@@ -37,7 +39,7 @@ public class AccountController(DataContext context, ITokenService tokenService) 
         {
             Username = user.UserName,
             Token = tokenService.CreateToken(user)
-        };
+        }; */
     }
 
     [HttpPost("Login")]
